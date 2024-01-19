de7dc89cfc5ad2deff8592ac106dea9a262ccf2c
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-01-19 19:32:22 +0800

    changed chaser bin capacity to 30

--- ChaserBin.js
+++ ChaserBin.js
@@ -3,7 +3,7 @@ import { Vehicle } from './Vehicle.js';
 export class ChaserBin extends Vehicle {
     constructor(x, y, images, paddock) {
         super(x, y, images, paddock);
-        this.storageCapacity = 50; // Specific to ChaserBin
+        this.storageCapacity = 30; // Specific to ChaserBin
         this.moveDelay = 3;
         this.currentLoad = 0;
     }
